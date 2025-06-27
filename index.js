6c6f94d45145e809ca4fe2b6d6f9aafd6bff0c24
Lucas <paragithub01@gmail.com>
2025-06-27 02:53:20 -0300

    Revisión de Motor de plantillas

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require('express')
-const path = require('path');
-const morgan = require('morgan');
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+require("dotenv").config({ path: "./.env" });
 
-const app = express()
-process.loadEnvFile()
-const port = process.env.PORT || 3000
+const app = express();
+process.loadEnvFile();
+const port = process.env.PORT || 3000;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // middlewares
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hola mundo</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hola mundo</h1>");
+});
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`)
-})
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
